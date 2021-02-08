3b5bee1291a920a29797e77df52bf3a35fe44471
ESMAT001 <esmat10.niazi@gmail.com>
2021-02-08 13:56:17 +0430

    decrement and reset functionality added

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -7,19 +7,31 @@ class Counter extends Component {
             count: 0
         }
         this.increment = this.increment.bind(this);
+        this.decrement = this.decrement.bind(this);
+        this.reset = this.reset.bind(this);
     }
     increment() {
-        this.setState((prevState) => {
-            return ({
-                count: prevState.count+1
-            })
-        })
+        this.setState((prevState) => ({
+            count: prevState.count + 1
+        }))
+    }
+    decrement() {
+        this.setState((prevState) => ({
+            count: prevState.count - 1
+        }))
+    }
+    reset() {
+        this.setState((prevState) => ({
+            count:0
+        }))
     }
     render() {
         return (
             <div>
-                <h2>counter - {this.state.count}</h2>
+                <h2>   {this.state.count}</h2>
                 <button onClick={this.increment}>+1</button>
+                <button onClick={this.decrement}>-1</button>
+                <button onClick={this.reset}>Reset</button>
             </div>
         )
     }
