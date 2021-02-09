8964b790bfcf5875e7a216fd29536d53f9abec8f
ESMAT001 <esmat10.niazi@gmail.com>
2021-02-09 10:54:19 +0430

    form component 'select added'

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,21 +3,35 @@ import React, { Component } from 'react'
 class Form extends Component {
     constructor(props) {
         super(props)
-    
+
         this.state = {
-             email: ''
+            email: '',
+            username: 'Ahmad ',
+            option: '1'
         }
     }
-    handleEmail=(e)=>{
+    handleEmail = (e) => {
         this.setState({
-            email:e.target.value
+            email: e.target.value
+        })
+    }
+    handleSelect = e => {
+        this.setState({
+            option:e.target.value
         })
     }
     render() {
         return (
             <form>
-                <p>Email:{this.state.email}</p>
+                <p>Username : {this.state.username}</p>
                 <input type="text" value={this.state.email} onChange={this.handleEmail} />
+                <div>
+                    <select value={this.state.option} onChange={this.handleSelect}>
+                        <option value="1" >1</option>
+                        <option value="2" >2</option>
+                        <option value="3" >3</option>
+                    </select>
+                </div>
             </form>
         )
     }
